3488c281dd8e22206b460569b1d7333067d03e47
ACELEBI <ali.celebi@ynov.com>
2022-05-31 14:21:43 +0200

    Debut

---
+++ AAA/server.go
@@ -0,0 +1,22 @@
+package forum
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func HandleFunc() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/accueil.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	fs := http.FileServer(http.Dir("Static/"))
+	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import forum "forum/AAA"
+
+func main() {
+	forum.HandleFunc()
+}
