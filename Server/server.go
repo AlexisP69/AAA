0951862e607e404201343be5a2c0693a059a47e3
APOINSIGNON <alexis.poinsignon@ynov.com>
2022-06-01 13:49:40 +0200

    start private route api

--- Server/server.go
+++ Server/server.go
@@ -1,14 +1,58 @@
 package forum
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
-func HandleFunc() {
+type MyUsers struct {
+	name     string `json:name`
+	email    string `json:email`
+	password string `json:password`
+	//Picture  string `json:picture`
+}
+
+func HandleFunc(db *sql.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("Page/HomePage.html", "Static/HomePage.css"))
+		template := template.Must(template.ParseFiles("Page/HomePage.html", "templates/footer.html", "templates/navbar.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/Register.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, db)
+			return
+		}
+	})
+	http.HandleFunc("/registerApi", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{\"test\":\"tata\"}"))
+		var Users MyUsers
+
+		body, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(body, &Users)
+		fmt.Println(Users)
+
+		if Users.name == "test" {
+			w.Write([]byte("{\"test\":\"tata\"}"))
+			return
+		}
+
+		// Requete SQL
+		// Scan requête
+		// JSON.Marshal
+		w.Write([]byte("{\"test\":\"toto\"}"))
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/Login.html"))
 		if r.Method != http.MethodPost {
 			template.Execute(w, "")
 			return
